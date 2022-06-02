b0878651279d8428f11fb77e0feb6c3bc1b6f369
Jengas2015 <arejang@gmail.com>
2022-06-02 17:09:09 -0500

    6/2 changes
    added separate animation for mobile menu so opacity is set to 1.

--- src/main.js
+++ src/main.js
@@ -2,12 +2,15 @@ const btnHamburger= document.querySelector('#btnHamburger');
 const body=document.querySelector('body');
 const header = document.querySelector('.header');
 const fadeElems = document.querySelectorAll('.has-fade');
+const fadeMenu = document.querySelector('.has-fade-menu');
 
 btnHamburger.addEventListener('click', function() {
 
     if(header.classList.contains('open')) {// Close Hamburger Menu
         body.classList.remove('noscroll');
         header.classList.remove('open');
+        fadeMenu.classList.add('fade-out-menu');
+        fadeMenu.classList.remove('fade-in-menu');
         fadeElems.forEach(function(element) {
             element.classList.add('fade-out');
             element.classList.remove('fade-in');
@@ -17,6 +20,8 @@ btnHamburger.addEventListener('click', function() {
      else { //Open Hamburger Menu
         body.classList.add('noscroll');
         header.classList.add('open');
+        fadeMenu.classList.remove('fade-out-menu');
+        fadeMenu.classList.add('fade-in-menu');
         fadeElems.forEach(function(element) {
             element.classList.remove('fade-out');
             element.classList.add('fade-in');
